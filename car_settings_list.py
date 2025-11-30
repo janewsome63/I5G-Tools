398c54e1e042f2de81be38939f42a8f61dc2194e
cdemeritt <165437571+cdemeritt@users.noreply.github.com>
2025-11-29 23:07:23 -0500

    Added Indy Pro 2000 and USF2000 to list
    And started adding other cars, commented out for now

--- car_settings_list.py
+++ car_settings_list.py
@@ -1,14 +1,153 @@
+# Cars that have been commented out with "?" as an entry
+# are just to start the car entry. In-car limits have not
+# been checked for those cars yet.
+
 car_settings = {
+    # int(4): {               # Pro Mazda
+    #     "name": "[Legacy] Pro Mazda",
+    #     "?"
+    # },
+    # int(26): {              # C6.R GT1
+    #     "name": "Chevrolet Corvette C6.R GT1",
+    #     "?"
+    # },
+    # int(29): {              # IR-05
+    #     "name": "[Legacy] Dallara IR-05",
+    #     "?"
+    # },
+    # int(41): {              # CTS-V
+    #     "name": "Cadillac CTS-V Racecar",
+    #     "?"
+    # },
+    # int(57): {              # DW12
+    #     "name": "[Legacy] Dallara DW12",
+    #     "?"
+    # },
+    # int(64): {              # DBR9 GT1
+    #     "name": "Aston Martin DBR9 GT1",
+    #     "?"
+    # },
+    # int(70): {              # C7 DP
+    #     "name": "Chevrolet Corvette C7 Daytona Prototype",
+    #     "?"
+    # },
+    # int(76): {              # Audi 90 GTO
+    #     "name": "Audi 90 GTO",
+    #     "?"
+    # },
+    # int(98): {              # R18
+    #     "name": "Audi R18 LMP1",
+    #     "?"
+    # },
     int(99): {              # IR-18
         "name": "Dallara IR-18 Indycar",
         "weight_jacker": [-20, 20],
         "front_roll_bar": [1, 6],
         "rear_roll_bar": [1, 6],
         "fuel_map": [1, 8],
+        # "brake_bias": "?"
     },
+    # int(106): {             # F3
+    #     "name": "Dallara F3",
+    #     "?"
+    # },
+    # int(109): {             # M8 GTE
+    #     "name": "BMW M8 GTE",
+    #     "?"
+    # },
+    # int(112): {             # RS 3 LMS
+    #     "name": "Audi RS 3 LMS TCR",
+    #     "?"
+    # },
+    int(120): {             # Indy Pro 2000
+        "name": "Tatuus PM-18 Indy Pro 2000",
+        # "brake_bias": "?"
+    },
+    int(121): {             # USF2000
+        "name": "Tatuus USF-17 USF2000",
+        # "brake_bias": "?"
+    },
+    # int(122): {             # M4 F82 GT4 - 2018
+    #     "name": "BMW M4 F82 GT4 - 2018",
+    #     "?"
+    # },
+    # int(127): {             # C8.R GTE
+    #     "name": "Chevrolet Corvette C8.R GTE",
+    #     "?"
+    # },
+    # int(128): {             # P217
+    #     "name": "Dallara P217 LMP2"
+    #     "TC1": 
+    #     "TC2": 
+    # },
+    # int(129): {             # iR-01
+    #     "name": "Dallara iR-01",
+    #     "?"
+    # },
+    # int(132): {             # M4 GT3 EVO
+    #     "name": "BMW M4 GT3 EVO",
+    #     "?"
+    # },
+    # int(150): {             # AM GT4
+    #     "name": "Aston Martin Vantage GT4",
+    #     "?"
+    # },
+    # int(159): {             # BMW M Hybrid
+    #     "name": "BMW M Hybrid V8 GTP",
+    #     "?"
+    # },
+    # int(168): {             # V-Series.R GTP
+    #     "name": "Cadillac V-Series.R GTP",
+    #     "?"
+    # },
+    # int(170): {             # ARX-06
+    #     "name": "Acura ARX-06 GTP",
+    #     "?"
+    # },
+    # int(176): {             # R8 LMS EVO II GT3
+    #     "name": "Audi R8 LMS EVO II GT3",
+    #     "?"
+    # },
+    # int(184): {             # Z06 GT3.R
+    #     "name": "Chevrolet Corvette Z06 GT3.R",
+    #     "?"
+    # },
+    # int(189): {             # M4 G82 GT4 EVO
+    #     "name": "BMW M4 G82 GT4 EVO",
+    #     "?"
+    # },
+    # int(194): {             # NSX GT3 EVO 22
+    #     "name": "Acura NSX GT3 EVO 22",
+    #     "?"
+    # },
+    # int(195): {             # M2 CS
+    #     "name": "BMW M2 CS Racing",
+    #     "?"
+    # },
+    # int(198): {             # Chevy ARCA
+    #     "name": "ARCA Chevrolet SS"
+    #     "?"
+    # },
+    # int(199): {             # Mustang ARCA
+    #     "name": "ARCA Ford Mustang",
+    #     "?"
+    # },
+    # int(200): {             # Camry ARCA
+    #     "name": "ARCA Toyota Camry",
+    #     "?"
+    # },
+    # int(204): {             # Mustang GT4
+    #     "name": "Ford Mustang GT4",
+    #     "?"
+    # },
     int(205): {             # IL-15
         "name": "Dallara IL-15 Indy NXT",
         "front_roll_bar": [1, 5],
-        "rear_roll_bar": [1, 5]
-    }
+        "rear_roll_bar": [1, 5],
+        # "brake_bias": "?"
+    },
+    # int(206): {             # AM GT3 EVO
+    #     "name": "Aston Martin Vantage GT3 EVO",
+    #     "?"
+    # },
 }
