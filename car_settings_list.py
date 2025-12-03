1c425df6b913b02103a2508c0db310ba7b7bc9bd
cdemeritt <165437571+cdemeritt@users.noreply.github.com>
2025-12-02 23:06:21 -0500

    Added more commented out cars to list

--- car_settings_list.py
+++ car_settings_list.py
@@ -7,6 +7,14 @@ car_settings = {
     #     "name": "[Legacy] Pro Mazda",
     #     "?"
     # },
+    # int(5): {               # Legends Ford '34 Coupe
+    #     "name": "Legends Ford '34 Coupe",
+    #     "?"
+    # },
+    # int(25): {              # Lotus 79
+    #     "name": "Lotus 79",
+    #     "?"
+    # },
     # int(26): {              # C6.R GT1
     #     "name": "Chevrolet Corvette C6.R GT1",
     #     "?"
@@ -15,24 +23,116 @@ car_settings = {
     #     "name": "[Legacy] Dallara IR-05",
     #     "?"
     # },
+    # int(30): {              # Ford Mustang FR500S
+    #     "name": "Ford Mustang FR500S",
+    #     "?"
+    # },
+    # int(39): {             # HPD ARX 01c
+    #     "name": "HPD ARX 01c LMP2",
+    #     "?"
+    # },
+    # int(40): {              # Ford GT GT2
+    #     "name": "Ford GT GT2",
+    #     "?"
+    # },
     # int(41): {              # CTS-V
     #     "name": "Cadillac CTS-V Racecar",
+    #     "?"
+    # },
+    # int(42): {              # Lotus 49
+    #     "name": "Lotus 49",
+    #     "?"
+    # },
+    # int(44): {              # Optima
+    #     "name": "Kia Optima",
     #     "?"
     # },
     # int(57): {              # DW12
     #     "name": "[Legacy] Dallara DW12",
     #     "?"
     # },
+    # int(59): {              # Ford GT GT3
+    #     "name": "Ford GT GT3",
+    #     "?"
+    # },
     # int(64): {              # DBR9 GT1
     #     "name": "Aston Martin DBR9 GT1",
+    #     "?"
+    # },
+    # int(67): {              # Global Mazda MX-5 Cup
+    #     "name": "Global Mazda MX-5 Cup",
     #     "?"
     # },
     # int(70): {              # C7 DP
     #     "name": "Chevrolet Corvette C7 Daytona Prototype",
     #     "?"
     # },
+    # int(74): {              # Formula Renault 2.0
+    #     "name": "Formula Renault 2.0",
+    #     "?"
+    # },
     # int(76): {              # Audi 90 GTO
     #     "name": "Audi 90 GTO",
+    #     "?"
+    # },
+    # int(78): {             # Dirt Late Model - Limited
+    #     "name": "Dirt Late Model - Limited",
+    #     "?"
+    # },
+    # int(79): {             # Dirt Spreet Stock
+    #     "name": "Dirt Spreet Stock",
+    #     "?"
+    # },
+    # int(80): {             # Dirt Sprint Car - 305
+    #     "name": "Dirt Sprint Car - 305",
+    #     "?"
+    # },
+    # int(81): {             # Ford Fiesta RS WRC
+    #     "name": "Ford Fiesta RS WRC",
+    #     "?"
+    # },
+    # int(82): {               # Dirt Legends Ford '34 Coupe
+    #     "name": "Dirt Legends Ford '34 Coupe",
+    #     "?"
+    # },
+    # int(83): {             # Dirt Late Model - Pro
+    #     "name": "Dirt Late Model - Pro",
+    #     "?"
+    # },
+    # int(84): {             # Dirt Late Model - Super
+    #     "name": "Dirt Late Model - Super",
+    #     "?"
+    # },
+    # int(85): {             # Dirt Sprint Car - 360
+    #     "name": "Dirt Sprint Car - 360",
+    #     "?"
+    # },
+    # int(86): {             # Dirt Sprint Car - 410
+    #     "name": "Dirt Sprint Car - 410",
+    #     "?"
+    # },
+    # int(87): {             # Dirt Sprint Car - 360 Non-Winged
+    #     "name": "Dirt Sprint Car - 360 Non-Winged",
+    #     "?"
+    # },
+    # int(89): {             # Dirt Sprint Car - 410 Non-Winged
+    #     "name": "Dirt Sprint Car - 410 Non-Winged",
+    #     "?"
+    # },
+    # int(92): {              # Ford GTE
+    #     "name": "Ford GTE",
+    #     "?"
+    # },
+    # int(93): {             # Ferrari 488 GTE
+    #     "name": "Ferrari 488 GTE",
+    #     "?"
+    # },
+    # int(95): {             # Dirt UMP Modified
+    #     "name": "Dirt UMP Modified",
+    #     "?"
+    # },
+    # int(96): {             # Dirt Midget
+    #     "name": "Dirt Midget",
     #     "?"
     # },
     # int(98): {              # R18
@@ -47,6 +147,10 @@ car_settings = {
         "fuel_map": [1, 8],
         # "brake_bias": "?"
     },
+    # int(105): {              # Formula Renault 3.5
+    #     "name": "Formula Renault 3.5",
+    #     "?"
+    # },
     # int(106): {             # F3
     #     "name": "Dallara F3",
     #     "?"
@@ -84,16 +188,60 @@ car_settings = {
     #     "name": "Dallara iR-01",
     #     "?"
     # },
+    # int(131): {             # Dirt Big Block Modified
+    #     "name": "Dirt Big Block Modified",
+    #     "?"
+    # },
     # int(132): {             # M4 GT3 EVO
     #     "name": "BMW M4 GT3 EVO",
+    #     "?"
+    # },
+    # int(133): {              # Lamborghini Huracán GT3 EVO
+    #     "name": "Lamborghini Huracán GT3 EVO",
+    #     "?"
+    # },
+    # int(134): {             # Dirt 358 Modified
+    #     "name": "Dirt 358 Modified",
+    #     "?"
+    # },
+    # int(142): {              # Formula Vee
+    #     "name": "Formula Vee",
+    #     "?"
+    # },
+    # int(144): {             # Ferrari 488 GT3 Evo 2020
+    #     "name": "Ferrari 488 GT3 Evo 2020",
+    #     "?"
+    # },
+    # int(146): {             # Hyundai Elantra N TCR
+    #     "name": "Hyundai Elantra N TCR",
+    #     "?"
+    # },
+    # int(147): {             # Honda Civic Type R TCR
+    #     "name": "Honda Civic Type R TCR",
+    #     "?"
+    # },
+    # int(148): {             # FIA F4
+    #     "name": "FIA F4",
     #     "?"
     # },
     # int(150): {             # AM GT4
     #     "name": "Aston Martin Vantage GT4",
     #     "?"
     # },
+    # int(153): {             # Hyundai Veloster N TCR
+    #     "name": "Hyundai Veloster N TCR",
+    #     "?"
+    # },
     # int(159): {             # BMW M Hybrid
     #     "name": "BMW M Hybrid V8 GTP",
+    #     "?"
+    # },
+    # int(164): {             # Late Model Stock
+    #     "name": "Late Model Stock",
+    #     "?"
+    # },
+    # int(165): {             # Ligier JS P320
+    #     "name": "Ligier JS P320 LMP3",
     #     "?"
     # },
     # int(168): {             # V-Series.R GTP
@@ -104,16 +252,44 @@ car_settings = {
     #     "name": "Acura ARX-06 GTP",
     #     "?"
     # },
+    # int(173): {             # Ferrari 296 GT3
+    #     "name": "Ferrari 296 GT3",
+    #     "?"
+    # },
     # int(176): {             # R8 LMS EVO II GT3
     #     "name": "Audi R8 LMS EVO II GT3",
+    #     "?"
+    # },
+    # int(180): {             # Dirt Micro Sprint Car - Winged
+    #     "name": "Dirt Micro Sprint Car - Winged",
+    #     "?"
+    # },
+    # int(181): {             # Dirt Micro Sprint Car - Non-Winged
+    #     "name": "Dirt Micro Sprint Car - Non-Winged",
+    #     "?"
+    # },
+    # int(182): {             # Dirt Outlaw Micro Sprint Car - Winged
+    #     "name": "Dirt Outlaw Micro Sprint Car - Winged",
+    #     "?"
+    # },
+    # int(183): {             # Dirt Outlaw Micro Sprint Car - Non-Winged
+    #     "name": "Dirt Outlaw Micro Sprint Car - Non-Winged",
     #     "?"
     # },
     # int(184): {             # Z06 GT3.R
     #     "name": "Chevrolet Corvette Z06 GT3.R",
     #     "?"
     # },
+    # int(185): {             # Ford Mustang GT3
+    #     "name": "Ford Mustang GT3",
+    #     "?"
+    # },
     # int(189): {             # M4 G82 GT4 EVO
     #     "name": "BMW M4 G82 GT4 EVO",
+    #     "?"
+    # },
+    # int(193): {             # Dirt Mini Stock
+    #     "name": "Dirt Mini Stock",
     #     "?"
     # },
     # int(194): {             # NSX GT3 EVO 22
@@ -122,6 +298,10 @@ car_settings = {
     # },
     # int(195): {             # M2 CS
     #     "name": "BMW M2 CS Racing",
+    #     "?"
+    # },
+    # int(196): {             # Ferrari 499P
+    #     "name": "Ferrari 499P LMH",
     #     "?"
     # },
     # int(198): {             # Chevy ARCA
@@ -134,6 +314,18 @@ car_settings = {
     # },
     # int(200): {             # Camry ARCA
     #     "name": "ARCA Toyota Camry",
+    #     "?"
+    # },
+    # int(201): {             # Gen 4 Chevrolet Monte Carlo - 2003
+    #     "name": "Gen 4 Chevrolet Monte Carlo - 2003",
+    #     "?"
+    # },
+    # int(202): {             # Gen 4 Ford Taurus - 2003
+    #     "name": "Gen 4 Ford Taurus - 2003",
+    #     "?"
+    # },
+    # int(203): {             # Ferrari 296 Challenge
+    #     "name": "Ferrari 296 Challenge",
     #     "?"
     # },
     # int(204): {             # Mustang GT4
